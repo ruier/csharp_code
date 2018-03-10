60ed7b1b569cc86583671081462a97322a94921f
Royce <zuoruier@gmail.com>
2018-03-10 16:28:49 +0800

    c sharp code

---
+++ csharp_code/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, C # from console");
+            Console.ReadKey();
+        }
+    }
+}
